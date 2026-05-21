d78865c00aabfd33c3ac35cd1da82e173e4e3157
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-05-21 14:56:33 +0700

    Improve register screen and  user list UI

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,40 +1,120 @@
 import { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet,} from 'react-native';
 
-import {createUserWithEmailAndPassword,} from 'firebase/auth';
-import { auth } from '../firebase/config';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+} from 'react-native';
 
-import {doc, setDoc, serverTimestamp,} from 'firebase/firestore';
-import { db } from '../firebase/config';
+import * as ImagePicker from 'expo-image-picker';
+import { createUserWithEmailAndPassword,} from 'firebase/auth';
+import { doc, setDoc, serverTimestamp,} from 'firebase/firestore';
+import { ref, uploadBytes, getDownloadURL,} from 'firebase/storage';
+import { auth, db, storage,} from '../firebase/config';
 
-export default function LoginScreen({ navigation }) {
-
+export default function RegisterScreen({navigation}) {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [photoURL, setPhotoURL] = useState(null);
+
+  const pickImage = async () => {
+    try {
+      const permission =
+        await ImagePicker
+          .requestMediaLibraryPermissionsAsync();
+      if (!permission.granted) {
+        alert('Perlu izin galeri');
+        return;
+      }
+
+      const result =
+        await ImagePicker
+          .launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 0.8,
+          });
+
+      if (!result.canceled) {
+        uploadImage(
+          result.assets[0].uri
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const uploadImage = async (
+    imageUri
+  ) => {
+    try {
+      const response =
+        await fetch(imageUri);
+
+      const blob =
+        await response.blob();
+
+      const storageRef = ref(
+        storage,
+        `profiles/${Date.now()}.jpg`
+      );
+
+      await uploadBytes(
+        storageRef,
+        blob
+      );
+
+      const downloadURL =
+        await getDownloadURL(storageRef);
+      setPhotoURL(downloadURL);
+    } catch (error) {
+      console.log(error);
+      alert(
+        'Firebase Storage buth upgrade plan'
+      );
+    }
+  };
 
   const handleRegister = async () => {
     if (!email || !password) {
-      alert('Email dan password wajib diisi');
+      alert(
+        'Email dan password wajib diisi'
+      );
       return;
     }
 
     try {
-      //Buat akun auth
+
       const userCredential =
         await createUserWithEmailAndPassword(
           auth,
           email,
           password
         );
-      const user = userCredential.user;
-      //Simpan profile ke Firestore
+
+      const user =
+        userCredential.user;
+
       await setDoc(
         doc(db, 'users', user.uid),
         {
+          name:
+            name ||
+            email.split('@')[0],
           email: user.email,
           isOnline: true,
           createdAt: serverTimestamp(),
-          photoURL:`https://api.dicebear.com/7.x/initials/png?seed=${email}`,
+          photoURL:
+            photoURL ||
+            `https://api.dicebear.com/7.x/initials/png?seed=${
+              name || email
+            }`,
         }
       );
       alert('Register berhasil');
@@ -46,16 +126,45 @@ export default function LoginScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-
       <Text style={styles.title}>
         Register
       </Text>
+      
+      <TouchableOpacity
+        style={styles.avatarContainer}
+        onPress={pickImage}
+      >
+
+        <Image
+          source={{
+            uri:
+              photoURL ||
+              `https://api.dicebear.com/7.x/initials/png?seed=${
+                name || email || 'User'
+              }`
+          }}
+          style={styles.avatar}
+        />
+
+        <Text style={styles.uploadText}>
+          Tap untuk ganti foto
+        </Text>
+
+      </TouchableOpacity>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nama"
+        value={name}
+        onChangeText={setName}
+      />
 
       <TextInput
         style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
+        autoCapitalize="none"
       />
 
       <TextInput
@@ -70,21 +179,29 @@ export default function LoginScreen({ navigation }) {
         style={styles.button}
         onPress={handleRegister}
       >
+
         <Text style={styles.buttonText}>
           Register
         </Text>
+
       </TouchableOpacity>
 
       <TouchableOpacity
-        onPress={() => navigation.navigate('Login')}
+        onPress={() =>
+          navigation.navigate('Login')
+        }
       >
+
         <Text style={styles.link}>
           Sudah punya akun? Login
         </Text>
+
       </TouchableOpacity>
 
     </View>
+
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -93,6 +210,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     padding: 20,
+    backgroundColor: 'white',
+  },
+
+  avatarContainer: {
+    alignItems: 'center',
+    marginBottom: 25,
+  },
+
+  avatar: {
+    width: 110,
+    height: 110,
+    borderRadius: 55,
+  },
+
+  uploadText: {
+    marginTop: 10,
+    color: 'gray',
+    fontSize: 13,
   },
 
   title: {
@@ -104,22 +239,25 @@ const styles = StyleSheet.create({
 
   input: {
     borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 10,
+    borderColor: '#ddd',
+    borderRadius: 12,
     padding: 14,
     marginBottom: 15,
+    backgroundColor: '#f9f9f9',
   },
 
   button: {
     backgroundColor: '#007AFF',
     padding: 15,
-    borderRadius: 10,
+    borderRadius: 12,
     alignItems: 'center',
+    marginTop: 10,
   },
 
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
+    fontSize: 16,
   },
 
   link: {
--- src/screens/UserListScreen.js
+++ src/screens/UserListScreen.js
@@ -4,79 +4,9 @@ import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image} from 'react-
 import { signOut } from 'firebase/auth';
 import { collection, onSnapshot, doc, updateDoc, serverTimestamp } from 'firebase/firestore';
 import { db, auth } from '../firebase/config';
-import * as ImagePicker from 'expo-image-picker';
-import { ref, uploadBytes, getDownloadURL,} from 'firebase/storage';
-import { storage } from '../firebase/config';
 
 export default function UserListScreen({navigation}) {
   const [users, setUsers] = useState([]);
-
-  const pickImage = async () => {
-    try {
-      const permission =
-        await ImagePicker
-          .requestMediaLibraryPermissionsAsync();
-      if (!permission.granted) {
-        alert('Izin galeri diperlukan');
-        return;
-      }
-
-      const result =
-        await ImagePicker
-          .launchImageLibraryAsync({
-            mediaTypes: ['images'],
-            allowsEditing: true,
-            aspect: [1, 1],
-            quality: 0.8,
-          });
-      if (result.canceled) return;
-      uploadProfilePhoto(
-        result.assets[0].uri
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const uploadProfilePhoto = async (
-      imageUri
-    ) => {
-      try {
-        const response =
-          await fetch(imageUri);
-        const blob =
-          await response.blob();
-        const storageRef = ref(
-          storage,
-          `profiles/${auth.currentUser.uid}.jpg`
-        );
-
-        await uploadBytes(
-          storageRef,
-          blob
-        );
-
-        const downloadURL =
-          await getDownloadURL(storageRef);
-
-        await updateDoc(
-          doc(
-            db,
-            'users',
-            auth.currentUser.uid
-          ),
-          {
-            photoURL: downloadURL,
-          }
-        );
-        alert('Foto profil berhasil diupload');
-      } catch (error) {
-        console.log(error);
-        alert(
-          'Firebase Storage buth upgrade plan'
-        );
-      }
-    };
 
   const handleLogout = async () => {
     try {
@@ -116,20 +46,14 @@ export default function UserListScreen({navigation}) {
 
 return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Users
+      <View style={styles.header}>
+      <Text style={styles.appTitle}>
+        MyChatApp
       </Text>
-
-      <TouchableOpacity
-        style={styles.uploadButton}
-        onPress={pickImage}
-      >
-
-        <Text style={styles.uploadButtonText}>
-          Upload Foto Profil
-        </Text>
-
-      </TouchableOpacity>
+      <Text style={styles.subTitle}>
+        Chats
+      </Text>
+    </View>
 
       <FlatList
         data={users}
@@ -145,54 +69,66 @@ return (
                 }
               )
             }
+
             style={styles.userCard}
           >
+
             {
               item.photoURL && (
+
                 <Image
                   source={{
                     uri: item.photoURL,
                   }}
-
-                  style={{
-                    width: 50,
-                    height: 50,
-                    borderRadius: 25,
-                    marginBottom: 10,
-                  }}
+                  style={styles.avatar}
                 />
 
               )
             }
 
-            <Text style={styles.userEmail}>
-              {item.email}
-            </Text>
+            <View style={styles.userInfo}>
 
-            <Text
-              style={[
-                styles.statusText,
-                item.isOnline
-                  ? styles.onlineText
-                  : styles.offlineText
-              ]}
-            >
-              {
-                item.isOnline
-                  ? 'Online'
-                  : 'Offline'
-              }
-            </Text>
+              <Text style={styles.userName}>
+                {item.name || 'Unknown'}
+              </Text>
+
+              <Text
+                style={[
+                  styles.statusText,
+
+                  item.isOnline
+                    ? styles.onlineText
+                    : styles.offlineText
+                ]}
+              >
+
+                {
+                  item.isOnline
+                    ? 'Online'
+                    : 'Offline'
+                }
+
+              </Text>
+
+              <Text style={styles.userEmail}>
+                {item.email}
+              </Text>
+
+            </View>
+
           </TouchableOpacity>
         )}
       />
 
       <TouchableOpacity
+        style={styles.logoutButton}
         onPress={handleLogout}
       >
-        <Text style={styles.logoutText}>
+
+        <Text style={styles.logoutButtonText}>
           Logout
         </Text>
+
       </TouchableOpacity>
     </View>
   );
@@ -205,56 +141,89 @@ const styles = StyleSheet.create({
     padding: 20,
   },
 
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
+  header: {
+      marginBottom: 25,
+    },
 
-  userCard: {
-    padding: 15,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 10,
-    marginBottom: 10,
-    backgroundColor: 'white',
-  },
+    appTitle: {
+      fontSize: 32,
+      fontWeight: 'bold',
+    },
 
-  userEmail: {
-    fontSize: 16,
-    fontWeight: '500',
-  },
+    subTitle: {
+      marginTop: 5,
+      fontSize: 18,
+      color: 'gray',
+    },
 
-  statusText: {
-    marginTop: 5,
-  },
+    userCard: {
+      flexDirection: 'row',
+      alignItems: 'center',
 
-  onlineText: {
-    color: 'green',
-  },
+      padding: 15,
 
-  offlineText: {
-    color: 'gray',
-  },
+      backgroundColor: 'white',
 
-  logoutText: {
-    marginTop: 20,
-    color: 'red',
-    textAlign: 'center',
-    fontWeight: 'bold',
-  },
+      borderRadius: 15,
 
-  uploadButton: {
-    backgroundColor: '#007AFF',
-    padding: 12,
-    borderRadius: 10,
-    marginBottom: 20,
-    alignItems: 'center',
-  },
+      marginBottom: 12,
 
-  uploadButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
+      shadowColor: '#000',
+      shadowOpacity: 0.05,
+      shadowRadius: 5,
 
+      elevation: 2,
+    },
+
+    avatar: {
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+    },
+
+    userInfo: {
+      marginLeft: 15,
+      flex: 1,
+    },
+
+    userName: {
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+
+    userEmail: {
+      marginTop: 4,
+      color: 'gray',
+      fontSize: 13,
+    },
+
+    statusText: {
+      marginTop: 2,
+      fontSize: 13,
+    },
+
+    onlineText: {
+      color: 'green',
+    },
+
+    offlineText: {
+      color: 'gray',
+    },
+
+    logoutButton: {
+      alignSelf: 'center',
+      marginTop: 10,
+      backgroundColor: '#ff3b30',
+      paddingVertical: 10,
+      paddingHorizontal: 30,
+      borderRadius: 999,
+      alignItems: 'center',
+      opacity: 0.9,
+    },
+
+    logoutButtonText: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 12,
+    },
 });
